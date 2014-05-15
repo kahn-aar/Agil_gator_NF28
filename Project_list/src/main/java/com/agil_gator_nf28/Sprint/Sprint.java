9ca648e3b125f585d2a32ef3d6117f29ec27c1f2
kahn-aar <nicolas.martinr@gmail.com>
2014-05-15 14:44:43 +0200

    Ajout des paramètres des classes
    Classes affectées : Tache, sousTache et sprint

--- Project_list/src/main/java/com/agil_gator_nf28/Sprint/Sprint.java
+++ Project_list/src/main/java/com/agil_gator_nf28/Sprint/Sprint.java
@@ -1,8 +1,26 @@
 package com.agil_gator_nf28.Sprint;
 
+import com.agil_gator_nf28.SousTaches.SousTache;
+import com.agil_gator_nf28.Taches.Tache;
+
+import java.util.List;
+
 /**
  * Created by Nicolas on 15/05/14.
  */
 public class Sprint {
 
+    private List<Tache> taches;
+
+    public Sprint(List<Tache> taches) {
+        this.taches = taches;
+    }
+
+    public List<Tache> getTaches() {
+        return taches;
+    }
+
+    public void setTaches(List<Tache> taches) {
+        this.taches = taches;
+    }
 }
--- Project_list/src/main/java/com/agil_gator_nf28/Taches/Tache.java
+++ Project_list/src/main/java/com/agil_gator_nf28/Taches/Tache.java
@@ -1,8 +1,61 @@
 package com.agil_gator_nf28.Taches;
 
+import com.agil_gator_nf28.SousTaches.SousTache;
+
+import java.util.List;
+
 /**
  * Created by Nicolas on 15/05/14.
  */
 public class Tache {
 
+    private List<SousTache> sousTaches;
+    private int priorite;
+    private int difficulte;
+    private int notifications;
+
+
+    public Tache(List<SousTache> sousTaches, int priorite, int difficulte, int notifications) {
+        this.sousTaches = sousTaches;
+        this.priorite = priorite;
+        this.difficulte = difficulte;
+        this.notifications = notifications;
+    }
+
+    public Tache(int priorite, int difficulte) {
+        this.priorite = priorite;
+        this.difficulte = difficulte;
+    }
+
+    public List<SousTache> getSousTaches() {
+        return sousTaches;
+    }
+
+    public void setSousTaches(List<SousTache> sousTaches) {
+        this.sousTaches = sousTaches;
+    }
+
+    public int getPriorite() {
+        return priorite;
+    }
+
+    public void setPriorite(int priorite) {
+        this.priorite = priorite;
+    }
+
+    public int getDifficulte() {
+        return difficulte;
+    }
+
+    public void setDifficulte(int difficulte) {
+        this.difficulte = difficulte;
+    }
+
+    public int getNotifications() {
+        return notifications;
+    }
+
+    public void setNotifications(int notifications) {
+        this.notifications = notifications;
+    }
 }
