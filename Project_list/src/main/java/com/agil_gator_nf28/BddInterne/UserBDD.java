9e60994c5355335a2d02380b7798e488ae552b46
kahn-aar <nicolas.martinr@gmail.com>
2014-06-05 18:33:29 +0200

    Possibilité d'ajout de membre au projet

--- Project_list/src/main/java/com/agil_gator_nf28/BddInterne/UserBDD.java
+++ Project_list/src/main/java/com/agil_gator_nf28/BddInterne/UserBDD.java
@@ -9,6 +9,9 @@ import com.agil_gator_nf28.User.ConnectedUser;
 import com.agil_gator_nf28.User.User;
 import com.agil_gator_nf28.constantes.AndroidConstantes;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Classe gérant la table des utilisateurs
  *
@@ -85,5 +88,76 @@ public class UserBDD extends GestionnaireBDD {
         Cursor c = bdd.rawQuery(query, null);
 
         return cursorToUser(c);
+    }
+
+    public List<User> getListUserOfAProject(int projectId) {
+        String query = "SELECT " + AndroidConstantes.TABLE_USER + "." + AndroidConstantes.COL_USER_ID + ", " + AndroidConstantes.COL_USER_NAME + ", " + AndroidConstantes.COL_USER_PRENOM + ", " + AndroidConstantes.COL_USER_EMAIL
+                + " FROM " + AndroidConstantes.TABLE_USER + " INNER JOIN " + AndroidConstantes.TABLE_USER_PROJET + " ON " + AndroidConstantes.COL_USER_PROJET_USER + " = " + AndroidConstantes.COL_USER_ID
+                + " WHERE " + AndroidConstantes.COL_USER_PROJET_PROJET + " = " + projectId + ";";
+        Cursor c = bdd.rawQuery(query, null);
+
+        return cursorToUsers(c);
+    }
+
+    private List<User> cursorToUsers(Cursor c) {
+        //si aucun élément n'a été retourné dans la requête, on renvoie null
+        if (c.getCount() == 0)
+            return new ArrayList<User>();
+
+        List<User> users = new ArrayList<User>();
+
+        //Sinon on se place sur le premier élément
+        c.moveToFirst();
+        //On créé un projet
+        while(!c.isAfterLast()) {
+            User user = new User();
+            //on lui affecte toutes les infos grâce aux infos contenues dans le Cursor
+            user.setId(c.getInt(NUM_COL_ID));
+            user.setName(c.getString(NUM_COL_NAME));
+            user.setFirstname(c.getString(NUM_COL_FIRST_NAME));
+            user.setEmail(c.getString(NUM_COL_EMAIL));
+
+            users.add(user);
+            c.moveToNext();
+        }
+
+        //On ferme le cursor
+        c.close();
+
+        //On retourne le projet
+        return users;
+    }
+
+    public List<User> getListUserOfNotAProject(int projectId) {
+        List<User> allUsers = new ArrayList<User>();
+        String query = "SELECT * FROM " + AndroidConstantes.TABLE_USER + ";";
+        Cursor c = bdd.rawQuery(query, null);
+
+        allUsers = cursorToUsers(c);
+
+        List<User> halfUsers = new ArrayList<User>();
+        query = "SELECT " + AndroidConstantes.TABLE_USER + "." + AndroidConstantes.COL_USER_ID + ", " + AndroidConstantes.COL_USER_EMAIL + ", " + AndroidConstantes.COL_USER_NAME + ", " + AndroidConstantes.COL_USER_PRENOM
+                + " FROM " + AndroidConstantes.TABLE_USER + " INNER JOIN " + AndroidConstantes.TABLE_USER_PROJET + " ON " + AndroidConstantes.COL_USER_PROJET_USER + " = " + AndroidConstantes.TABLE_USER + "." + AndroidConstantes.COL_USER_ID
+                + " WHERE " + AndroidConstantes.COL_USER_PROJET_PROJET + " = " + projectId + ";";
+        c = bdd.rawQuery(query, null);
+
+        halfUsers = cursorToUsers(c);
+        List<User> finalUsers = new ArrayList<User>();
+        boolean ok = true;
+        for (User user : halfUsers) {
+            System.out.println(user.getEmail());
+            for(User user2 : allUsers) {
+                System.out.println(user2.getEmail());
+                if (user.getEmail().equals(user2.getEmail())) {
+                    ok = false;
+                }
+            }
+            if (ok) {
+                finalUsers.add(user);
+            }
+            ok = true;
+        }
+
+        return finalUsers;
     }
 }
---
+++ Project_list/src/main/java/com/agil_gator_nf28/User/UserAdapter.java
@@ -0,0 +1,110 @@
+package com.agil_gator_nf28.User;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.agil_gator_nf28.BddInterne.UserProjetBDD;
+import com.agil_gator_nf28.Listeners.TacheGridListener;
+import com.agil_gator_nf28.SousTaches.SousTacheAdapter;
+import com.agil_gator_nf28.SousTaches.SousTacheEtat;
+import com.agil_gator_nf28.Taches.Tache;
+import com.agil_gator_nf28.agil_gator.DescriptifTaskActivity;
+import com.agil_gator_nf28.agil_gator.Page_projet;
+import com.agil_gator_nf28.agil_gator.R;
+import com.agil_gator_nf28.constantes.AndroidConstantes;
+
+import java.util.List;
+
+
+/**
+ * Created by Nicolas on 05/06/14.
+ */
+public class UserAdapter extends ArrayAdapter<User> {
+
+    private List<User> taches;
+    private LayoutInflater inflater;
+    private int projectId;
+
+    public UserAdapter(Context context, int resource, List<User> users, int projectId) {
+        super(context, resource);
+        inflater = LayoutInflater.from(context);
+        this.taches = users;
+        this.projectId = projectId;
+    }
+
+    @Override
+    public int getCount() {
+        return taches.size();
+    }
+
+    @Override
+    public User getItem(int position) {
+        return taches.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    private class ViewHolder {
+        TextView nom;
+        TextView prenom;
+        TextView email;
+    }
+
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+
+        final int pos = position;
+
+        if(convertView == null) {
+            holder = new ViewHolder();
+            // On lie les éléments au fichier ligne_de_la_listview.xml
+            convertView = inflater.inflate(R.layout.membre_layout, null);
+            // On lie les deux TextView déclarés précédemment à ceux du xml
+            holder.nom = (TextView)convertView.findViewById(R.id.nomUser);
+            holder.prenom = (TextView)convertView.findViewById(R.id.prenomUser);
+            holder.email = (TextView)convertView.findViewById(R.id.userEmail);
+
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+        // On défini ici le texte que doit contenir chacun des TextView
+        // Le premier affichera le numéro de l'élément (numéro de ligne)
+        holder.nom.setText(taches.get(position).getName());
+        holder.prenom.setText(taches.get(position).getFirstname());
+        holder.email.setText(taches.get(position).getEmail());
+
+        //Onclick sur la view
+
+        convertView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                UserProjetBDD userProjetBDD = new UserProjetBDD(getContext());
+                userProjetBDD.open();
+                userProjetBDD.insertProjet(taches.get(pos), (long) projectId);
+                userProjetBDD.close();
+
+                Intent intent = new Intent(getContext(), Page_projet.class);
+                intent.putExtra("user_login", String.valueOf(projectId));
+                getContext().startActivity(intent);
+            }
+        });
+
+        return convertView;
+    }
+
+}
